81ca4ef89f15cc1421977f681814d7bc4a7489c2
vaseekk <110334370+Vaseekk@users.noreply.github.com>
2024-05-29 12:34:49 +0200

    konec

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': '#7A9D8C',
+      },
+    },
   },
   plugins: [],
 }
